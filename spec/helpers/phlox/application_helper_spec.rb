d147705396412c9bfb3799b9ff0d7eeba4747649
Nicholas Cancelliere <ncancelliere@gmail.com>
2013-12-06 17:40:06 -0600

    added comment to test

--- spec/helpers/phlox/application_helper_spec.rb
+++ spec/helpers/phlox/application_helper_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 module Phlox
   describe ApplicationHelper do
+    # this is just a simple test for the initial CCRB
+    # once actual tests are developed this can be removed
     it "is true" do
       expect(true).to be true
     end
