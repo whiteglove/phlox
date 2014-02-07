70a1f0fd4d1efc80ccd92791327d2fe6c65494e2
Ethan Burrow <texasjusticar@gmail.com>
2014-02-07 11:11:26 -0600

    allow for configurable system user

--- app/models/phlox/login.rb
+++ app/models/phlox/login.rb
@@ -1,6 +1,10 @@
 module Phlox
   class Login < Base
 
+    # the parent app should configure a system user so
+    # to do system related api calls you can authorize by
+    #
+    # Phlox::Login.authorize(Phlox.system_user, Phlox.system_password)
     def self.authorize(username, password)
       response = post(:login, { username: username, password: password })
       obj = new(decode_body_from_response(response))
--- lib/phlox/engine.rb
+++ lib/phlox/engine.rb
@@ -1,6 +1,6 @@
 module Phlox
   class << self
-    mattr_accessor :site
+    mattr_accessor :site, :system_user, :system_password
   end
 
   def self.setup(&block)
