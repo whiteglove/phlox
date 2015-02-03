c260c8ce3455ba19cff05f20161b3a9c3c3b50e3
Adam Bruss <abruss@whiteglove.com>
2015-02-02 23:50:50 -0600

    Patients return an object via the find method

--- app/models/phlox/drchrono/patient.rb
+++ app/models/phlox/drchrono/patient.rb
@@ -1,40 +1,49 @@
 class Phlox::Drchrono::Patient < Phlox::Drchrono::Base
 
-  PATIENT_URL = "#{Phlox.drchrono_site}/api/patients"
-
   attr_accessor :attributes
 
   def initialize(attribs = {})
     @attributes = attribs
   end
 
-  # Assuming id is a unique identifier, this method replicates the ActiveRecord find method
-  def self.find(id)
-    results = JSON.parse(HTTParty.get(PATIENT_URL, headers: auth_header).response.body)["results"]
-    patient = results.select{ |patient| patient["id"] == id }.first
-    new(patient)
-  end
+  # DrChrono doesn't support PUTs... Have to find a workaround.
+  # def update_attributes(attribs = {})
+  #   results = JSON.parse(HTTParty.put(self.class.url, body: attribs.merge!('id' => @attributes['id']), headers: self.class.auth_header).response.body)
+  # end
 
-  def self.all
-    JSON.parse(HTTParty.get(PATIENT_URL, headers: auth_header).response.body)["results"]
-  end
+  class << self
 
-  def self.create(params = {})
-    body = {
-      'chart_id' => params[:chart_id],
-      'first_name' => params[:first_name],
-      'last_name' => params[:last_name],
-      'gender' => params[:gender],
-      'date_of_birth' => params[:dob],
-      'address' => params[:address],
-      'city' => params[:city],
-      'state' => params[:state],
-      'zip_code' => params[:zip],
-      'email' => params[:email],
-      'doctor' => params[:doctor],
-      'employer' => params[:employer],
-      'home_phone' => params[:phone]
-    }
-    patient = JSON.parse(HTTParty.post(PATIENT_URL, body: body, headers: auth_header).response.body)
+    # Assuming id is a unique identifier, this method replicates the ActiveRecord find method
+    def find(id)
+      results = JSON.parse(HTTParty.get(url, headers: auth_header).response.body)["results"]
+      new(results.select{ |patient| patient["id"] == id }.first)
+    end
+
+    def all
+      JSON.parse(HTTParty.get(url, headers: auth_header).response.body)["results"]
+    end
+
+    def create(params = {})
+      body = {
+        'chart_id' => params[:chart_id],
+        'first_name' => params[:first_name],
+        'last_name' => params[:last_name],
+        'gender' => params[:gender],
+        'date_of_birth' => params[:dob],
+        'address' => params[:address],
+        'city' => params[:city],
+        'state' => params[:state],
+        'zip_code' => params[:zip],
+        'email' => params[:email],
+        'doctor' => params[:doctor],
+        'employer' => params[:employer],
+        'home_phone' => params[:phone]
+      }
+      JSON.parse(HTTParty.post(url, body: body, headers: auth_header).response.body)
+    end
+
+    def url
+      "#{Phlox.drchrono_site}/api/patients"
+    end
   end
 end
