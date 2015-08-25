b5016ce4da5554888060b44d48a4c5c85fe0e89b
Adam Bruss <abruss@whiteglove.com>
2015-08-25 15:29:31 -0500

    Added delete and update methods

--- app/models/phlox/drchrono/patient.rb
+++ app/models/phlox/drchrono/patient.rb
@@ -1,16 +1,19 @@
 class Phlox::Drchrono::Patient < Phlox::Drchrono::Base
 
-  PATIENT_ATTRIBS = [:id, :first_name, :last_name, :date_of_birth, :gender, :address, :city, :state, :zip_code, :email, :home_phone]
+  PATIENT_ATTRIBS = [:id, :chart_id, :first_name, :last_name, :date_of_birth, :gender, :address, :city, :state, :zip_code, :email, :home_phone, :doctor]
 
   include ActiveModel::Validations
 
-  validates_presence_of :first_name, :last_name, :date_of_birth, :gender
+  validates_presence_of :first_name, :last_name, :date_of_birth, :gender, :doctor
+  validates_numericality_of :doctor
+  validates_inclusion_of :gender, in: %w( Male Female Other )
 
   attr_accessor *PATIENT_ATTRIBS
 
   def attributes
     {
       :id => id,
+      :chart_id => chart_id,
       :first_name => first_name,
       :last_name => last_name,
       :date_of_birth => date_of_birth,
@@ -20,7 +23,8 @@ class Phlox::Drchrono::Patient < Phlox::Drchrono::Base
       :state => state,
       :zip_code => zip_code,
       :email => email,
-      :home_phone => home_phone
+      :home_phone => home_phone,
+      :doctor => doctor
     }
   end
 
@@ -28,13 +32,7 @@ class Phlox::Drchrono::Patient < Phlox::Drchrono::Base
     PATIENT_ATTRIBS.each do |attrib|
       instance_variable_set("@#{attrib}", attribs[attrib])
     end
-    valid?
   end
-
-  # DrChrono doesn't support PUTs... Have to find a workaround.
-  # def update_attributes(attribs = {})
-  #   results = JSON.parse(HTTParty.put(self.class.url, body: attribs.merge!('id' => @attributes['id']), headers: self.class.auth_header).response.body)
-  # end
 
   class << self
 
@@ -59,27 +57,18 @@ class Phlox::Drchrono::Patient < Phlox::Drchrono::Base
       end
     end
 
-    def create(params = {})
+    def create(params)
+      valid_params?(params)
       params = Hash[params.map{ |k, v| [k.to_sym, v] }] # Convert all keys to symbols
       params[:chart_id] = SecureRandom.uuid
-      params[:doctor] = Phlox::Drchrono::Doctor.default_doctor
-      body = {
-        'chart_id' => params[:chart_id],
-        'first_name' => params[:first_name],
-        'last_name' => params[:last_name],
-        'gender' => params[:gender],
-        'date_of_birth' => params[:date_of_birth],
-        'address' => params[:address],
-        'city' => params[:city],
-        'state' => params[:state],
-        'zip_code' => params[:zip_code],
-        'email' => params[:email],
-        'doctor' => params[:doctor],
-        'employer' => params[:employer],
-        'home_phone' => params[:home_phone]
-      }
-      response = JSON.parse(HTTParty.post(url, body: body, headers: auth_header).response.body)
-      new(response.symbolize_keys)
+      body = {}
+      params.each {|k,v| body[k.to_s] = v}
+      patient = new(body.symbolize_keys)
+      if patient.valid?
+        response = JSON.parse(HTTParty.post(url, body: body, headers: auth_header).response.body) 
+        response["id"].present? ? patient.id = response["id"] : patient.add_drchrono_errors(response)
+      end
+      patient
     end
 
     def url
@@ -101,6 +90,40 @@ class Phlox::Drchrono::Patient < Phlox::Drchrono::Base
 
     def translate_search_param(key)
       ["first_name","last_name","email"].include?(key.to_s) ? "search" : key
+    end
+
+    def valid_params?(params)
+      invalid_params = []
+      params.each {|k,v| invalid_params << k unless PATIENT_ATTRIBS.include?(k)}
+      raise "Invalid params: #{invalid_params.join(", ")}" unless invalid_params.empty?
+      true
+    end
+  end
+
+  # Instance methods
+
+  def update_attributes(attribs)
+    Phlox::Drchrono::Patient.valid_params?(attribs)
+    response = JSON.parse(HTTParty.patch("#{self.class.url}/#{self.id}", body: attribs, headers: self.class.auth_header).response.body)
+    if response["id"].present?
+      attribs.each {|k,v| instance_variable_set("@#{k}", v)} 
+    else 
+      return response
+    end
+    self
+  end
+
+  def delete
+    response = HTTParty.delete("#{self.class.url}/#{self.id}", headers: self.class.auth_header).try(:response).try(:body)
+    if response.nil? 
+      return self.id
+    end
+    JSON.parse(response)
+  end
+
+  def add_drchrono_errors(response)
+    response.each do |k,v|
+      errors.add(k.to_sym, v)
     end
   end
 end
